110c2a257ece181418677b9c82a9518bad6b647d
AlexIlyin <se@alexilyin.com>
2016-03-25 11:59:46 +0300

    Toast added

--- app/src/main/java/com/alexilyin/android/a31_githubtest/MainActivity.java
+++ app/src/main/java/com/alexilyin/android/a31_githubtest/MainActivity.java
@@ -2,6 +2,7 @@ package com.alexilyin.android.a31_githubtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this, "Test it!", Toast.LENGTH_SHORT).show();
     }
 }
